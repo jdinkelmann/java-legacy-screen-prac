fe74c7947a1e4fbd04d04075110d6eefa3a43a99
Jason Dinkelmann <jdinkelmann@gmail.com>
2018-02-21 15:24:49 -0500

    Refactored out the Zipcode service, add test to cover if no location is found.  JobApplicant tests are green, service are red.

--- src/main/java/com/neopragma/legacy/service/ZipcodeService.java
+++ src/main/java/com/neopragma/legacy/service/ZipcodeService.java
@@ -1,5 +1,63 @@
 package com.neopragma.legacy.service;
 
-public class ZipcodeService {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class ZipcodeService {
+	public String findCityState(String zipCode) throws URISyntaxException, IOException{
+			URI uri = buildZipCodesDotComURI(zipCode);
+	        HttpGet request = new HttpGet(uri);
+	        CloseableHttpClient httpclient = HttpClients.createDefault();
+	        CloseableHttpResponse response = httpclient.execute(request);
+	        try {
+	            HttpEntity entity = response.getEntity();
+	            if (entity != null) {
+	              	BufferedReader rd = new BufferedReader(
+	                        new InputStreamReader(response.getEntity().getContent()));
+	           		StringBuffer result = new StringBuffer();
+	           		String line = "";
+	           		while ((line = rd.readLine()) != null) {
+	           			result.append(line);
+	       		    }
+	                int metaOffset = result.indexOf("<meta ");
+	                int contentOffset = result.indexOf(" content=\"Zip Code ", metaOffset);
+	                contentOffset += 19;
+	                contentOffset = result.indexOf(" - ", contentOffset);
+	                contentOffset += 3;
+	                int stateOffset = result.indexOf(" ", contentOffset);
+	                //city = result.substring(contentOffset, stateOffset);
+	                stateOffset += 1;
+	                //state = result.substring(stateOffset, stateOffset+2);
+	            } else {
+	            	//city = "";
+	            	//state = "";
+	            }
+	        } finally {
+	            response.close();
+	        }
+	        
+	        return "";
+	}
+
+	private URI buildZipCodesDotComURI(String zipCode) throws URISyntaxException {
+		URI uri = new URIBuilder()
+	            .setScheme("http")
+	            .setHost("www.zip-codes.com")
+	            .setPath("/search.asp")
+	            .setParameter("fld-zip", zipCode)
+	            .setParameter("selectTab", "0")
+	            .setParameter("srch-type", "city")
+	            .build();
+		return uri;
+	}
 }
--- src/test/java/com/neopragma/legacy/service/ZipcodeServiceIntegrationTest.java
+++ src/test/java/com/neopragma/legacy/service/ZipcodeServiceIntegrationTest.java
@@ -8,27 +8,32 @@ import java.net.URISyntaxException;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.neopragma.legacy.screen.JobApplicant;
-
 public class ZipcodeServiceIntegrationTest {
-	private JobApplicant jobApplicant;
+	private ZipcodeService service;
 	
 	@Before
 	public void beforeEach() {
-		jobApplicant = new JobApplicant();
+		service = new ZipcodeService();
 	}
 	
 	@Test
 	public void itFindsAddisonTexasBy5DigitZipCode() throws URISyntaxException, IOException {
-		jobApplicant.setZipCode("75001");
-		assertEquals("Addison", jobApplicant.getCity());
-		assertEquals("TX", jobApplicant.getState());
+		String actualLocation = service.findCityState("75001");
+		
+		assertEquals("Addison", actualLocation);
+		assertEquals("TX", actualLocation);
 	}
 	
 	@Test
 	public void itFindsMaranaArizonaBy9DigitZipCode() throws URISyntaxException, IOException {
-		jobApplicant.setZipCode("856585578");
-		assertEquals("Marana", jobApplicant.getCity());
-		assertEquals("AZ", jobApplicant.getState());
+		String actualLocation = service.findCityState("856585578");
+
+		assertEquals("Marana", actualLocation);
+		assertEquals("AZ", actualLocation);
+	}
+	
+	@Test(expected = IOException.class)
+	public void itThrowsAnExeptionIfNoLocationIsFound() throws URISyntaxException, IOException {
+		service.findCityState("1");
 	}
 }
